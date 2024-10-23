3247d181804ee48ebc71d49292cf2aca457901fa
Gabriel <moutga@gmail.com>
2024-10-23 09:33:31 -0300

    Definir estilos

---
+++ static/main.js
@@ -0,0 +1,5 @@
+const frases = [
+    "Madera recuperada, la mejor opción para muebles únicos",
+    "Diseñamos y fabricamos muebles de estilo industrial, combinando hierro y madera para crear piezas con personalidad",
+    "Tus proyectosen las mejores manos",
+]
